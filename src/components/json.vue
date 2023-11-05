fff61ebf6ef59b9fcec7e6a55a893964b2811f10
Yoav <yoav@resonai.com>
2023-11-05 22:14:03 +0200

    edit string entries

--- src/components/json.vue
+++ src/components/json.vue
@@ -1,12 +1,46 @@
 <script setup>
+  import { toValue, ref } from 'vue'
   import { useJson } from '../modules/json.js'
 
   const { jsonAsObject } = useJson()
   const breadcrumbs = [jsonAsObject]
+  const objects = ref([])
+  const strings = ref([])
+
+  parseTop()
 
   function back () {
     if (breadcrumbs.length > 1) {
       breadcrumbs.pop()
+      parseTop()
+    }
+  }
+
+  function drillDown (entry) {
+    const top = toValue(breadcrumbs[breadcrumbs.length - 1])
+    breadcrumbs.push(top[entry])
+    parseTop()
+  }
+
+  function updateString (entry, newValue) {
+    const top = toValue(breadcrumbs[breadcrumbs.length - 1])
+    top[entry.name] = newValue
+    jsonAsObject.value = { ...jsonAsObject.value }
+    parseTop()
+  }
+
+  function parseTop () {
+    objects.value = []
+    strings.value = []
+    const top = toValue(breadcrumbs[breadcrumbs.length - 1])
+    console.log(top, typeof top)
+    for (const property in top) {
+      console.log(property, typeof top[property])
+      if (typeof top[property] === 'object') {
+        objects.value.push(property)
+      } else if (typeof top[property] === 'string') {
+        strings.value.push({ name: property, value: top[property] })
+      }
     }
   }
 </script>
@@ -17,6 +51,17 @@
   <button type="button" @click="back">
     Back
   </button>
+  <li v-for="entry in objects" :key="entry">
+    <button type="button" @click="drillDown(entry)">
+      {{ entry }}
+    </button>
+  </li>
+  <li v-for="entry in strings" :key="entry">
+    {{ entry.name }}:
+    <input
+      :value="entry.value"
+      @blur="event => updateString(entry, event.target.value)">
+  </li>
 </template>
 
 <style scoped>
--- src/modules/json.js
+++ src/modules/json.js
@@ -4,17 +4,21 @@ const jsonAsString = ref('')
 const error = ref('')
 const jsonAsObject = ref({})
 
-export function useJson () {
-  watch(jsonAsString, () => {
-    try {
-      const obj = JSON.parse(jsonAsString.value)
-      jsonAsObject.value = obj
-      error.value = ''
-    } catch (err) {
-      jsonAsObject.value = {}
-      error.value = 'JSON is not well defined'
-    }
-  })
+watch(jsonAsString, () => {
+  try {
+    const obj = JSON.parse(jsonAsString.value)
+    jsonAsObject.value = obj
+    error.value = ''
+  } catch (err) {
+    jsonAsObject.value = {}
+    error.value = 'JSON is not well defined'
+  }
+})
 
+watch(jsonAsObject, () => {
+  jsonAsString.value = JSON.stringify(jsonAsObject.value, null, 2)
+})
+
+export function useJson () {
   return { jsonAsString, error, jsonAsObject }
 }
