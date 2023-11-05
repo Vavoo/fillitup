046c8303a1a2540212a6829a0fa3dad29e65aa9e
Yoav <yoav@resonai.com>
2023-11-05 20:30:58 +0200

    parse json to catch errors

--- src/modules/json.js
+++ src/modules/json.js
@@ -2,11 +2,18 @@ import { ref, watch } from 'vue'
 
 const jsonAsString = ref('')
 const error = ref('')
+const jsonAsObject = ref({})
 
 export function useJson () {
   watch(jsonAsString, () => {
-    console.log('json changed:', jsonAsString)
-    error.value = 'JSON is not well defined'
+    try {
+      const obj = JSON.parse(jsonAsString.value)
+      jsonAsObject.value = obj
+      error.value = ''
+    } catch (err) {
+      jsonAsObject.value = {}
+      error.value = 'JSON is not well defined'
+    }
   })
 
   return { jsonAsString, error }
